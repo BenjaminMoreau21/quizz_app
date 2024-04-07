c8b8212145d1afb77f1259651cc7d03e774f0426
Benjamin Moreau <benjamin.moreau49@gmail.com>
2024-04-07 17:20:35 +0200

    create and destroy passing tests

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -7,4 +7,26 @@ class ThemesController < ApplicationController
   def new
     @theme = Theme.new
   end
+
+  def create
+    @theme = Theme.new(theme_params)
+    @theme.user = current_user
+    if @theme.save
+      redirect_to theme_path(@theme)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @theme = Theme.find(params[:id])
+    @theme.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def theme_params
+    params.require(:theme).permit(:name)
+  end
 end
